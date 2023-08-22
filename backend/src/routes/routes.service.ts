c77cbbbb261c846f568bc16e5efa67ab840c44e0
gtrodrigues04 <grodrigues813@gmail.com>
2023-08-22 18:29:32 -0300

    desafio nestjs full cycle concluído

---
+++ backend/src/routes/routes.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+@Injectable()
+export class RoutesService {
+  constructor(private prismaService: PrismaService) {}
+
+   async create(createRouteDto: CreateRouteDto) {
+    return await this.prismaService.route.create({
+      data: {
+        name: createRouteDto.name,
+        source: {
+          name: 'Lavras, MG',
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        destination: {
+          name: 'Varginha, MG',
+          location: {
+            lat: 0,
+            lng: 0
+          },
+        }
+      }
+    });
+  }
+
+  findAll() {
+    return this.prismaService.route.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} route`;
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
